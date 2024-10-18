cc2c27061b010afafdf05ba3be7acb5a0c7d73c2
papasani.mohansrinivas <papasani.mohansrinivas@gmail.com>
2024-10-18 17:54:14 +0530

    lambda_deployable

--- nfl_demo/model_inference/views.py
+++ nfl_demo/model_inference/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # from model_inference.load_play_by_play import get_data
 # from apps import 
-from .tasks import my_task
+# from .tasks import my_task
+from model_inference.mass_simulate_compare_odds import sim_super_bowl
+from model_inference.models import results_table
 import json 
 from model_inference.apps import ModelInferenceConfig
 import uuid 
@@ -39,6 +41,11 @@ def index(request):
     # my_task(df_dict[week],co_eff,)
     uuid_ = uuid.uuid4()
 
-    my_task.delay(df_dict[week],co_eff,is_custom_sim,team_A,team_B,n_sims,week,run_id,f"{uuid_}")
+    # my_task.delay(df_dict[week],co_eff,is_custom_sim,team_A,team_B,n_sims,week,run_id,f"{uuid_}")
+
+    result_dict_ = sim_super_bowl(df_dict,co_eff,is_custom_sim,team_A=team_A, team_B=team_B, percentile_break=0.2, n_sims=n_sims, year=2024, week=week, excl_vs_before=True)
+    
+    obj = results_table(team_a_moneyline = result_dict_["team_a_moneyline"]  , team_b_moneyline = result_dict_["team_b_moneyline"]  , team_a_spread =  result_dict_["team_a_spread"], team_b_spread = result_dict_["team_b_spread"] , team_a_spread_line = result_dict_["team_a_spread_line"]  , team_b_spread_line = result_dict_["team_b_spread_line"] , team_a_totals = result_dict_["team_a_totals"] , team_b_totals = result_dict_["team_b_totals"] , team_a_totals_line = result_dict_["team_a_totals_line"]  , team_b_totals_line = result_dict_["team_b_totals_line"]  , home_team = team_A , away_team = team_B , run_id = run_uuid , uuid = uuid_)
+    obj.save()
 
     return HttpResponse(json.dumps({"home_team":team_A,"away_team":team_B,"uuid":f"{uuid_}","id":f"{run_id}"}))
